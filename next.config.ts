03344a723bcff58654cc23a19723c901dbe0ffd0
MuhammadSatriap <msatriapp1810@gmail.com>
2025-06-19 20:03:01 +0700

    fix image

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'jfqabsvncgnvpyfttdwa.supabase.co', // <-- GANTI DENGAN HOSTNAME ANDA
         port: '',
         pathname: '/storage/v1/object/public/**', // Izinkan semua gambar dari storage
-        unoptimized: true,
+        
       },
     ],
   },
